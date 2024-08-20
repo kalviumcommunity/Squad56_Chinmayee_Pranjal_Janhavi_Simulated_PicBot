6a709ae7b4e31e93d46e6358d7c7b39fdb138926
Pranjal <pranjalgosavi2507@gmail.com>
2024-08-20 15:02:48 +0530

    Created the main AI generator page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
 import './Pages/Login.jsx'
-import Login from './Pages/Login.jsx'
+// import Login from './Pages/Login.jsx'
 import Navbar from './Components/Navbar.jsx'
-import Home from './Pages/Home.jsx'
-import Signup from './Pages/Signup.jsx'
+// import Home from './Pages/Home.jsx'
+// import Signup from './Pages/Signup.jsx'
 import Generator from './Pages/Generator.jsx'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 function App() {
