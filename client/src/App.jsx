bdf64252fabbf71f3a3a14b2c21bc959d7d2290f
Pranjal <pranjalgosavi2507@gmail.com>
2024-08-20 17:44:53 +0530

    Created the Creators/ AboutUs page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import './App.css'
-import './Pages/Login.jsx'
+// import './Pages/Login.jsx'
 // import Login from './Pages/Login.jsx'
-import Navbar from './Components/Navbar.jsx'
+// import Navbar from './Components/Navbar.jsx'
 // import Home from './Pages/Home.jsx'
 // import Signup from './Pages/Signup.jsx'
-import Generator from './Pages/Generator.jsx'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Creators from './Pages/Creators.jsx'
+// import Generator from './Pages/Generator.jsx'
+// import {BrowserRouter, Route, Routes} from "react-router-dom"
 function App() {
 
   return (
     <>
+        <Creators/>
       {/* <Login/> */}
         {/* <Signup/> */}
-        <Generator/>
+        {/* <Generator/> */}
       {/* <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
+        // <Route path='/login' element={<Login/>}></Route>
       </Routes>
       </BrowserRouter> */}
     </>
