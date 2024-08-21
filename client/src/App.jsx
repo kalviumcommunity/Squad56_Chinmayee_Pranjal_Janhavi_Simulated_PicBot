cccaa8455492b63f75cf02c8f8c30b053d3d6cc8
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-08-21 15:05:03 +0530

    Features page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import './App.css'
 import Login from './Pages/Login.jsx'
 import Features from './Pages/Features.jsx'
-import Generator from './Pages/Generator.jsx'
-import Home from './Pages/Home.jsx'
-import Creators from './Pages/Creators.jsx'
-import Signup from './Pages/Signup.jsx'
+// import Generator from './Pages/Generator.jsx'
+// import Home from './Pages/Home.jsx'
+// import Creators from './Pages/Creators.jsx'
+// import Signup from './Pages/Signup.jsx'
 
 
 function App() {
 
   return (
     <>  
-      <Login/>
-      <Signup/>
-      <Home/>
-      <Generator/>
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      {/* <Home/> */}
+      {/* <Generator/> */}
       <Features/>
-      <Creators/>
+      {/* <Creators/> */}
     </>
   )
 }
